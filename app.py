d3b4e8ae7f2b6a868c07045d2b25027c7e8e95ad
賴柏瑄 <guavarepublic1@gmail.com>
2022-04-25 20:39:43 +0800

    test

--- app.py
+++ app.py
@@ -16,8 +16,7 @@ line_bot_api = LineBotApi('mCPd6yE2BF6p74QHjVd+SDl4tuYnGSBh/iFwQ6NmwSSAJR+NbYNAa
 # 必須放上自己的Channel Secret
 handler = WebhookHandler('87c5f4d4188a6f2372fcf27786692f93')
 
-line_bot_api.push_message('U7044af8ff4f5da1b940b535199766117', TextSendMessage(text='你可以開始了'))
-
+line_bot_api.push_message('U7044af8ff4f5da1b940b535199766117')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -40,7 +39,6 @@ def callback():
 
  
 #訊息傳遞區塊
-##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
